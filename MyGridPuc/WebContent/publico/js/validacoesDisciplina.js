44ab24dc786beb20e5f3daa6203216da680d724b
Wesley Calixto dos Reis <wesley.c.reis@gmail.com>
2014-10-31 21:18:03 -0200

    Fim da Definição do Layoute

--- MyGridPuc/WebContent/publico/js/validacoesDisciplina.js
+++ MyGridPuc/WebContent/publico/js/validacoesDisciplina.js
@@ -1,12 +1,28 @@
 //desmarca todos os radios deixando apenas o selecionado marcado
 function desmarcar(radio){
-	var formulario = document.getElementById('formprincipal');
+	var formulario = document.getElementById('formulario');
 	var campo;
 	for(var item=0; item<formulario.length; item++){
     	campo = formulario[item];
         if(campo.type=='radio' && campo.name!=radio.name){    
         	campo.checked = false;
     	}
+    }
+}
+
+//Limpa todos os Campos do Formulario
+function limpaForm(){
+	var formulario = document.getElementById('formulario');
+	var campo;
+	for(var item=0; item<formulario.length; item++){
+    	campo = formulario[item];
+        if(campo.type=='radio'){    
+        	campo.checked = false;
+    	}
+        if(campo.type=='text'){    
+        	campo.value = "";
+    	}
+
     }
 }
 
@@ -18,7 +34,7 @@ function selecionarItem(campoHidden,radioSelecionado){
 
 //verifica se algum item esta selecionado
 function validarSelecionado(){
-	var formulario = document.getElementById('formprincipal');
+	var formulario = document.getElementById('formulario');
 	var campo;
 	
 	for(var item=0; item<formulario.length; item++){
@@ -34,7 +50,7 @@ function validarSelecionado(){
 //valida o formulario
 function validarFormulario(){
 	
-	var formulario = document.getElementById('formprincipal');
+	var formulario = document.getElementById('formulario');
 	if(formulario.nome.value.lenght()<0){
 		alert('O campo nome deve ser preenchido').
 		formulario.nome.focus();
@@ -46,5 +62,5 @@ function validarFormulario(){
 
 
 function retornaIdDisciplina(){
-	return document.getElementById('formprincipal:formulario:idDisciplina').value;
+	return document.getElementById('formulario:idDisciplina').value;
 }
